09e785972972c9ea1beac5ba61c7d5c1a904e0ad
nuri park <nuri_park2@tmax.co.kr>
2024-10-23 15:06:40 +0900

    [feat] prod 프로파일인 경우 HTTPS 프로토콜만 허용하도록 config 추가

--- src/main/java/com/securityex/config/ProjectSecurityProdConfig.java
+++ src/main/java/com/securityex/config/ProjectSecurityProdConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,18 +13,18 @@ import org.springframework.security.web.authentication.password.HaveIBeenPwnedRe
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
-@Profile("!prod")
+@Profile("prod")
 @Configuration
-public class ProjectSecurityConfig {
+public class ProjectSecurityProdConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.requiresChannel(rcc -> rcc.anyRequest().requiresInsecure())
+        http.requiresChannel(rcc -> rcc.anyRequest().requiresSecure())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((requests) -> requests
                 .requestMatchers("/myAccount", "/myBalance","/myCards").authenticated()
                 .requestMatchers("/notices", "/contact","/register" ,"/error").permitAll()
         );
-//        http.formLogin(flc -> flc.disable());
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
